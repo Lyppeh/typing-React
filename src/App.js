0b37b100c750691f9cb846b497e15d86d8dbbc1f
Lyppeh <fellypemnascimento@gmail.com>
2021-09-17 14:48:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="valid-keys">
+                <span className="matched">emer</span>
+                <span className="remainder">son</span>
+            </div>
+            <div className="typed-keys">fsfsadfasdfsadsa</div>
+            <div className="completed-words">
+                <ol>
+                    <li>cidade</li>
+                    <li>cidade</li>
+                    <li>municipio</li>
+                    <li>cidade</li>
+                    <li>estadooo</li>
+                </ol>
+            </div>
+        </div>
+    )
 }
 
 export default App;
